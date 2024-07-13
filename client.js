d9bcd4369f4bcbe2595589c716d65a8bf3ce3fd4
sankalpashish022 <sankalpashish022@gmail.com>
2024-07-13 13:38:00 +0530

    change-1

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
-const { Redis } = require("ioredis");
-const client = redis.createClient({
-    host: 'redis',  
-    port: 6379,
-  });
+const redis = require('redis');
 
+const client = redis.createClient({
+    host: 'redis',
+    port: 6379,
+});
 
 client.on('error', (err) => {
     console.error('Redis connection error:', err);
@@ -12,4 +12,5 @@ client.on('error', (err) => {
 client.on('connect', () => {
     console.log('Redis connected');
 });
+
 module.exports = client;
